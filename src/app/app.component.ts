ef4a8672156f92a44b163f75ec02f6666c14b61d
taha <2345672@cegepmontpetit.ca>
2024-10-04 21:23:57 -0400

    FCT_ajout de album dans TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Projettp1';
+  Albums: Album[] = [];
+  Nomartiste?: string = "";
+  
+ 
+
+  constructor(public httpClient: HttpClient) {}
+
+  async searchlisteArtiste(): Promise<void> {
+    let x = await lastValueFrom(this.httpClient.get<any>("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + this.Nomartiste + "&api_key=8f630f1f91e115680f041428891d246b&format=json"));
+    console.log(x);
+    this.Albums = [];
+    for (let i = 0; i < x.topalbums.album.length; i++) {
+      let objAlbum = x.topalbums.album[i];
+      let album = new Album(objAlbum.artist.name, objAlbum.name, objAlbum.image[2]["#text"]);
+      this.Albums.push(album);
+    }
+    console.log(this.Albums); 
+  }
+
+
 }
+
+class Album {
+  constructor(public artist: string, public name: string, public image: string) {}
+}
+
